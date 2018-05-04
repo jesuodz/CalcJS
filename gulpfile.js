4a2a4e98fe62ac29c6c57a61c57c399c1075e930
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-05-04 17:40:44 -0400

    Set gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,84 @@
+const gulp = require('gulp'),
+    del = require('del');
+    rename = require('gulp-rename'),
+    htmlmin = require('gulp-htmlmin'),
+    cleanCSS = require('gulp-clean-css'),
+    uglify = require('gulp-uglify'),
+    browserSync = require('browser-sync').create(),
+    deploy = require('gulp-gh-pages');
 
+const src = 'app/';
+const libs = 'src/lib/';
+const dist = 'dist/';
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+})
+
+gulp.task('html', () => {
+    return gulp.src(src + 'html/*.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true,
+        }))
+        .pipe(rename('index.html'))
+        .pipe(gulp.dest(dist))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+})
+
+gulp.task('css', () => {
+    return gulp.src( src + 'stylesheets/*.css')
+        .pipe(cleanCSS())
+        .pipe(rename('main.css'))
+        .pipe(gulp.dest(dist))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('uglifyjs', () => {
+
+    var js_libs = gulp.src( libs + '*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(dist));
+
+    var scripts = gulp.src( src + 'scripts/*.js')
+        .pipe(uglify())
+        .pipe(rename('index.js'))
+        .pipe(gulp.dest(dist));
+    
+    return (js_libs, scripts)
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('fonts', () => {
+    return gulp.src(src + 'fonts/**/*')
+        .pipe(gulp.dest(dist + 'fonts'))
+});
+
+gulp.task('browserSync', () => {
+    browserSync.init({
+        server: {
+            baseDir: 'dist'
+        }
+    })
+});
+
+gulp.task('watch', ['browserSync', 'css', 'html', 'uglifyjs', 'fonts'], () => {
+    gulp.watch(src + 'stylesheets/*.css', ['css']);
+    gulp.watch(
+        [src + 'scripts/*.js', libs + '*.js'],
+        ['uglifyjs']);
+    gulp.watch(src + 'html/*.html', ['html'])
+});
+
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*')
+    .pipe(ghPages());
+});
+
+gulp.task('default', ['watch', 'clean']);
