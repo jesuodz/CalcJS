f06ebd241e5d11847b63ed529a9dc75e62cfe74a
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-05-08 09:56:48 -0400

    Fix user cases 4, 5 and 7

--- app/scripts/calc.js
+++ app/scripts/calc.js
@@ -3,24 +3,47 @@ $(document).ready( function() {
     var operationButton = $('button:not([value])');
     var display = $('#display');
     var input = [];
+    var flag = true;
 
     var operations = {
         'addButton' : '+',
         'subtractButton' : '-',
         'divideButton' : '/',
         'multiplyButton' : '*'
+    };
+
+    function debugFunction() {
+        console.log(input);
+        console.log(flag);
+    }
+    function evalInput() {
+        var result = eval(input.join(''));
+        clear();
+        input.push(result);
+        display.val(result);
+    }
     function isNumeric(item) {
         return !isNaN(parseFloat(item)) && isFinite(item);
+    }
+    function clear() {
+        flag = true;
+        input = [];
     }
 
     numberButton.click(function() {
         var operating = $(this).attr("value");
-        
+
         if (!isNumeric(input[input.length - 1])) {
             display.val("");
         }
+        if (!flag) {
+            display.val("");
+            clear();
+        }
+
         display.val(display.val() + operating);
         input.push(String(operating));
+        debugFunction();
     });
 
     operationButton.click(function() {
@@ -30,17 +53,23 @@ $(document).ready( function() {
         switch(operator) {
             case "clearButton":
                 display.val("");
-                input = [];
+                clear();
                 debugFunction();
                 break;
             case "equalsButton":
                 result = eval(input.join(''));
                 display.val(result);
-                input = [];
+                clear();
                 input.push(result);
+
+                flag = false;
+
+                debugFunction();
                 break;
             default:
+                evalInput();
                 input.push(operations[operator]);
+                debugFunction();
                 break;
         }
     });
