e4f128b680a59296d6efedff23f370f8055cc7d3
Jesus Ordosgoitty <jesuodz@gmail.com>
2018-05-08 08:06:18 -0400

    Fix bug: change display after result

--- app/scripts/calc.js
+++ app/scripts/calc.js
@@ -9,27 +9,35 @@ $(document).ready( function() {
         'subtractButton' : '-',
         'divideButton' : '/',
         'multiplyButton' : '*'
+    function isNumeric(item) {
+        return !isNaN(parseFloat(item)) && isFinite(item);
     }
 
     numberButton.click(function() {
         var operating = $(this).attr("value");
         
+        if (!isNumeric(input[input.length - 1])) {
+            display.val("");
+        }
         display.val(display.val() + operating);
         input.push(String(operating));
     });
 
     operationButton.click(function() {
         var operator = $(this).attr("id");
+        var result;
 
         switch(operator) {
             case "clearButton":
                 display.val("");
                 input = [];
+                debugFunction();
                 break;
             case "equalsButton":
-                console.log(input);
-                var joinedInput = input.join('');
-                display.val(eval(joinedInput));
+                result = eval(input.join(''));
+                display.val(result);
+                input = [];
+                input.push(result);
                 break;
             default:
                 input.push(operations[operator]);
